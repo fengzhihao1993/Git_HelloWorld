34eeee265ac8bc292a130d198aa7f6080d9c8ffa
fengzhihao1993 <18516880584@163.com>
2015-09-07 22:47:10 +0800

    Update HelloWorld.java
    Error by Net

--- src/com/atguigu/git/HelloWorld.java
+++ src/com/atguigu/git/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("PC update 01");
 		System.out.println("Net update 01");
+		
+		System.out.println("Error by Net");
 	}
 	
 }
